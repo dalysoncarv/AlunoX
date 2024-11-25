97ae8f1913ad4994af46b88d9f4b68e7d9d39513
dalysoncarv <dalyson.carv@gmail.com>
2024-11-25 20:24:10 -0300

    primeiro commit

---
+++ Js/site.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+
+const cloneContainer = container.cloneNode(true);
+cloneContainer.id = 'dark-container';
+document.body.appendChild(cloneContainer);
+cloneContainer.classList.remove('active');
+
+const toggleIcons = document.querySelectorAll('.toggle-icon');
+const icons = document.querySelectorAll('.toggle-icon i');
+const darkContainer = document.querySelector('#dark-container');
+const darkContainerImg = document.querySelector('#dark-container .home-img img');
+
+darkContainerImg.src = '/Img/imgDark.png';
+
+toggleIcons.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+
+        toggleIcons.forEach(toggles => {
+            toggles.classList.add('disabled');
+
+            setTimeout(() => {
+                toggles.classList.remove('disabled');
+            }, 1500);
+        });
+
+        icons.forEach(icon => {
+            icon.classList.toggle('bx-sun');
+        });
+
+        container.classList.toggle('active');
+        darkContainer.classList.toggle('active');
+
+    });
+});
